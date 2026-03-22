488861cb5e727e54e52ee8e5e75d053e066760f7
shindeakshay8597 <shindeakshay77778@gmail.com>
2026-03-23 02:30:41 +0530

    Initial commit

---
+++ iis-ci-cd-poc/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using iis_ci_cd_poc.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace iis_ci_cd_poc.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
